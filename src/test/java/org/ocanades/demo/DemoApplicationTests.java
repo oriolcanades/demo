8ae8cb0f7e942e2576d16ff8f6b6a1a3dcb15b06
Oriol Canadés <83498869+oriolcanadesin2@users.noreply.github.com>
2025-01-09 18:28:29 +0100

    Initial commit

---
+++ src/test/java/org/ocanades/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package org.ocanades.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// This test is empty because it is just to check that the context loads
+	}
+
+}
